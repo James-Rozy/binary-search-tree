accca5079608970b27614a5c2629b099a250bd0d
James Rozsypal <jimmerxl@gmail.com>
2022-08-25 11:14:18 -0400

    Add complete Node class and empty Tree class with an empty main iife for testing trees

---
+++ binary-search-tree.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.leftChild = null;
+    this.rightChild = null;
+  }
+}
+
+class Tree {
+  constructor() {}
+}
+
+const main = (() => {})();
